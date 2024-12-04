2f81a743df9c6a76319af3609a0ca196131751f9
Eaelllfaust <eaelllfaust@gmail.com>
2024-12-04 14:59:02 +0100

    changes

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,66 +1,82 @@
-import React from 'react'
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
-import { Link } from 'react-router-dom'
-import { get_banners } from '../store/reducers/homeReducer'
-import { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Link } from "react-router-dom";
+import { get_banners } from "../store/reducers/homeReducer";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Banner = () => {
+  const dispatch = useDispatch();
+  const { banners } = useSelector((state) => state.home);
 
-    const dispatch = useDispatch()
-    const { banners } = useSelector(state => state.home)
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 1
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 1
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 1
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    }
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-    useEffect(() => {
-        dispatch(get_banners())
-    }, [])
+  useEffect(() => {
+    dispatch(get_banners());
+  }, []);
+
+
+  const CustomLeftArrow = ({ onClick }) => {
+
+    if (window.innerWidth <= 785) return null;
     return (
-        <div className='w-full'>
-            <div className='w-[85%] lg:w-[90%] mx-auto br'>
-                <div className='w-full flex flex-wrap'>
-                    <div className='w-full'>
-                        <div className=''>
-                            <Carousel
-                                autoPlay={true}
-                                infinite={true}
-                                arrows={true}
-                                showDots={false}
-                                responsive={responsive}
-                            >
-                               
-                                     <Link className='lg-md:h-[440px] h-auto w-full block' >
-                                     <img className='nex_banner_img'   src="http://localhost:3001/images/banner/banner_nexigo_gray.svg" alt="" />
-                                    </Link>
-                                     <Link className='lg-md:h-[440px] h-auto w-full block' >
-                                     <img className='nex_banner_img'   src="http://localhost:3001/images/banner/banner_nexigo_primary.svg" alt="" />
-                                    </Link>
+    <></>
+    );
+  };
 
-                                
-                            </Carousel>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="w-full">
+      <div className="w-[85%] lg:w-[90%] mx-auto br">
+        <div className="w-full flex flex-wrap">
+          <div className="w-full">
+            <div className="">
+              <Carousel
+                autoPlay={true}
+                infinite={true}
+                arrows={true}
+                showDots={false}
+                responsive={responsive}
+                customLeftArrow={<CustomLeftArrow />}
+              >
+                <Link className="lg-md:h-[440px] h-auto w-full block">
+                  <img
+                    className="nex_banner_img"
+                    src="http://localhost:3001/images/banner/banner_nexigo_gray.svg"
+                    alt=""
+                  />
+                </Link>
+                <Link className="lg-md:h-[440px] h-auto w-full block">
+                  <img
+                    className="nex_banner_img"
+                    src="http://localhost:3001/images/banner/banner_nexigo_primary.svg"
+                    alt=""
+                  />
+                </Link>
+              </Carousel>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,20 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { GrMail } from "react-icons/gr";
-import { IoIosCall } from "react-icons/io";
-import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-import {
-  FaLinkedinIn,
-  FaFacebookF,
-  FaUser,
-  FaLock,
-  FaList,
-} from "react-icons/fa";
-import {
-  AiOutlineTwitter,
-  AiFillGithub,
-  AiFillHeart,
-  AiFillShopping,
-} from "react-icons/ai";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -68,38 +52,40 @@ const Headers = () => {
     <div className="w-full py-5">
       <div className="w-[85%] lg:w-[90%] mx-auto">
         <div className="flex w-full flex-wrap justify-between md-lg:gap-8">
-          
-            <img
-              className="w-[80px] h-auto"
-              src="http://localhost:3001/images/nexigolarge.svg"
-              alt="logo"
-            />
-          
+          <img
+            className="w-[80px] h-auto"
+            src="http://localhost:3001/images/nexigolarge.svg"
+            alt="logo"
+          />
           <div className="w-8/12 pl-8 md-lg:pl-0 md-lg:w-full">
             <div className="flex flex-wrap w-full justify-between items-center md-lg:gap-6">
               <div className="w-8/12 md-lg:w-full">
                 <div className="flex h-[50px] items-center relative gap-5">
-                  <div className="relative  after:h-[25px]">
+                  <div className="relative after:h-[25px]">
                     <Select onValueChange={(value) => setCategory(value)}>
                       <SelectTrigger className="w-[130px] text-slate-100 font-semibold bg-transparent px-2 h-full outline-0 border-none">
                         <SelectValue placeholder="All Categories" />
                       </SelectTrigger>
                       <SelectContent className="nex_drop">
-                        {categorys.map((c, i) => (
-                          <SelectItem key={i} value={c.name}>
-                            {c.name}
-                          </SelectItem>
-                        ))}
+                        {categorys
+                          .slice()
+                          .sort((a, b) =>
+                            a.name.localeCompare(b.name)
+                          ) // Sort alphabetically by category name
+                          .map((c, i) => (
+                            <SelectItem className='hover:text-white cursor-pointer' key={i} value={c.name}>
+                              {c.name}
+                            </SelectItem>
+                          ))}
                       </SelectContent>
                     </Select>
                   </div>
                   <Input
-                    className="w-full placeholder-white  dark:bg-black text-slate-100 outline-0 px-3 h-full input_nex"
+                    className="w-full placeholder-white dark:bg-black text-slate-100 outline-0 px-3 h-full input_nex"
                     onChange={(e) => setSearchValue(e.target.value)}
                     type="text"
                     placeholder="Search for products, merchants or categories"
                   />
-
                   <button
                     onClick={search}
                     className="bg-white nex_button font-semibold"
